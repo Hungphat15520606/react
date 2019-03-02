bfb7375d89e6c2a5e70b1eaa93d7049614016e1e
Phạm Tấn Phát <22995627+phamtanphat@users.noreply.github.com>
2019-03-02 13:58:21 +0700

    Add files via upload

---
+++ src/Word.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class Word extends Component {
+  render() {
+    return (
+      <div>
+          <h3 className="btn btn-success">One</h3>
+          <h3>Mot</h3>
+      </div>
+    );
+  }
+}
+
