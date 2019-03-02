dd5d547ede6178cd40a214c9b68c090217197fd9
Phạm Tấn Phát <22995627+phamtanphat@users.noreply.github.com>
2019-03-02 15:30:27 +0700

    Update Word.js

--- src/Word.js
+++ src/Word.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
 
 export class Word extends Component {
+  getButton() {
+    if (this.props.wordInfo.isMemorized) {
+      return <button className="btn btn-success">Forgot</button>
+    } else {
+      return <button className="btn btn-danger">isMemorized</button>
+    }
+  }
   render() {
+    const { en , vn , isMemorized } = this.props.wordInfo;
     return (
-      <div>
-          <h3 style={{color : 'green' ,margin : 10}}>{this.props.en}</h3>
-          <h3>{this.props.vn}</h3>
+      <div className="word">
+          <div className="word-container">
+            <h3 className="text-success">{en}</h3>
+            <h3 className="text-danger">
+              {isMemorized ? '----' : vn}
+            </h3>
+          </div>
+          <div className="btn-container">
+              <button
+                className={isMemorized ? 'btn btn-success' : 'btn btn-danger'}>
+                {isMemorized ? 'Forgot' : 'Memorized'}
+              </button>
+              <button
+                className="btn btn-warning">
+                Remove
+              </button>
+          </div>
       </div>
     );
   }
